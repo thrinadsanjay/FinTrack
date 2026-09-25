83d849e54eaf88deafd51c2465a6f700854d756e
Admin <svcansible@gmail.com>
2026-09-25 11:11:23 +0530

    Cleanup Main Branch

--- app/db/mongo.py
+++
@@ -1,5 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from app.core.config import settings
-
-client = AsyncIOMotorClient(settings.FT_MONGO_URI)
-db = client[settings.FT_MONGO_DB_NAME]
